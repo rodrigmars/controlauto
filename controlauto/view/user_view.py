046d1f10ab6a3e4accf5adfc854903f8864910fa
rodrigmars <rodrigmars@gmail.com>
2023-05-06 21:27:12 -0300

    Ajuste no reset de cores e remoção de comentários

--- controlauto/view/user_view.py
+++ controlauto/view/user_view.py
@@ -14,10 +14,12 @@ class UserView(UserPresenter, User):
         self.frmUser = tk.Frame(root, pady=50)
         self.frmUser.pack()
 
+        # Variables
         self.name_text = tk.StringVar(self.frmUser, value=None)
         self.email_text = tk.StringVar(self.frmUser, value=None)
         self.telephone_text = tk.StringVar(self.frmUser, value=None)
 
+        # Nome
         self.name_label = tk.Label(self.frmUser, text="Nome")
         self.name_label.grid(row=0, column=0, columnspan=2, sticky='nsew')
 
@@ -28,7 +30,7 @@ class UserView(UserPresenter, User):
         self.name_notify = tk.Label(self.frmUser, text="*")
         self.name_notify.grid(row=1, column=1, sticky='nsew')
 
-
+        # Email
         self.email_label = tk.Label(self.frmUser, text="Email")
         self.email_label.grid(row=2, column=0, sticky='nsew')
 
@@ -38,17 +40,20 @@ class UserView(UserPresenter, User):
         self.email_notify = tk.Label(self.frmUser, text="*")
         self.email_notify.grid(row=3, column=1, sticky='nsew')
 
+        # Telefone
         self.telephone_label = tk.Label(self.frmUser, text="Telefone")
         self.telephone_label.grid(row=4, column=0, sticky='nsew')
 
-        self.telephone_entry = tk.Entry(self.frmUser, textvariable=self.telephone_text)
+        self.telephone_entry = tk.Entry(
+            self.frmUser, textvariable=self.telephone_text)
         self.telephone_entry.grid(row=5, column=0, sticky='nsew')
 
         self.telephone_notify = tk.Label(self.frmUser, text="*")
         self.telephone_notify.grid(row=5, column=1, sticky='nsew')
 
-
-        self.add_button = tk.Button(self.frmUser, text="Adicionar", command=self.add)
+        # Button
+        self.add_button = tk.Button(
+            self.frmUser, text="Adicionar", command=self.add)
         self.add_button.grid(row=6, column=0, sticky='nsew')
 
     def add(self) -> None:
@@ -66,28 +71,31 @@ class UserView(UserPresenter, User):
 
         if self.name_text.get().strip() == "":
             self.name_entry.focus()
-            # self.name_entry.configure(bg="yellow")
+            self.name_entry.configure({"background": "yellow"})
+            self.name_notify.configure({"foreground": "red"})
             return False
 
         # if self.name_text.get().__len__() < 1:
         #     self.name_entry.focus()
         #     # self.name_entry.configure(bg="yellow")
         #     return False
-
+        self.reset()
         return True
 
     def disable(self):
         pass
-        # self.B2.configure(state=tk.DISABLED, background='cadetblue')
-        # self.Focus.configure(state=tk.DISABLED, background='cadetblue')
 
     def enable(self):
         pass
-        # self.B2.configure(state=tk.NORMAL,
-        #                   background=self.B1.cget('background'))
-        # self.Focus.configure(state=tk.NORMAL,
 
-        #                      background=self.B1.cget('background'))
+    def reset(self):
+        self.name_entry.configure({"background": "white"})
+        self.email_entry.configure({"background": "white"})
+        self.telephone_entry.configure({"background": "white"})
+
+        self.name_notify.configure({"foreground": "black"})
+        self.email_notify.configure({"foreground": "black"})
+        self.telephone_notify.configure({"foreground": "black"})
 
     def clear(self):
         self.name_text.set("")
