ecaf701e0438e8b4f3e340dfab93fbb6d50c49b8
rodrigmars <rodrigmars@gmail.com>
2023-05-09 10:45:50 -0300

    Ajustes na integração do frame view user ao app

--- controlauto/components/NameComponent.py
+++ controlauto/components/NameComponent.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 
 
-class NameComponent(tk.Entry):
+class NameEntryComponent(tk.Entry):
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, min_digits:int, max_digits: int,  *args, **kwargs) -> None:
+
         super().__init__(*args, **kwargs)
 
+        self.min_digits = min_digits
+        self.total_digits = max_digits
         self.digits: str = ""
 
         self.bind("<KeyPress>", self.onKeyDown)
@@ -19,7 +22,10 @@ class NameComponent(tk.Entry):
 
     def onKeyDown(self, event):
 
-        if len(self.get_digits()) >= 5:
+        if str(event.char).isalpha() is not True:
+            return "break"
+
+        if len(self.get_digits()) >= self.total_digits:
             return "break"
 
         self.set_digits(self.get_digits() + str(event.char))
--- controlauto/view/user_view copy.py
+++ controlauto/view/user_view copy.py
@@ -3,73 +3,72 @@ from presenter.user_presenter import UserPresenter
 from model.User import User
 from model.EntryPhoneNumber import EntryPhoneNumber
 
+class UserView(UserPresenter, User):
 
+    def __init__(self, root) -> None:
 
-class UserView(tk.Frame):
+        super().__init__()
 
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+        self.BACKGROUND_YELLOW:dict = {"background": "yellow"}
+        self.FOREGROUND_YELLOW:dict = {"foreground": "red"}
 
-        self.BACKGROUND_YELLOW: dict = {"background": "yellow"}
-        self.FOREGROUND_YELLOW: dict = {"foreground": "red"}
-
-        self.BACKGROUND_DEFAULT: dict = {"background": "white"}
-        self.FOREGROUND_DEFAULT: dict = {"foreground": "black"}
+        self.BACKGROUND_DEFAULT:dict = {"background": "white"}
+        self.FOREGROUND_DEFAULT:dict = {"foreground": "black"}
         # configure(text_font = ('verdana', 8)
 
+        self.frmUser = tk.Frame(root, pady=50)
+        self.frmUser.pack()
+
         # Variables
-        self.name_text = tk.StringVar(self, value=None)
-        self.email_text = tk.StringVar(self, value=None)
-        self.telephone_text = tk.StringVar(self, value=None)
+        self.name_text = tk.StringVar(self.frmUser, value=None)
+        self.email_text = tk.StringVar(self.frmUser, value=None)
+        self.telephone_text = tk.StringVar(self.frmUser, value=None)
 
         # Nome
-        self.name_label = tk.Label(self, text="Nome")
+        self.name_label = tk.Label(self.frmUser, text="Nome")
         self.name_label.grid(row=0, column=0, columnspan=2, sticky='nsew')
 
-        self.name_entry = tk.Entry(self, textvariable=self.name_text, width=35)
+        self.name_entry = tk.Entry(self.frmUser, textvariable=self.name_text, width=35)
         self.name_entry.grid(row=1, column=0, sticky='nsew')
         self.name_entry.focus()
 
-        self.name_notify = tk.Label(self, text="*")
+        self.name_entry.bind("<Key>", self.event_key)
+
+
+        self.name_notify = tk.Label(self.frmUser, text="*")
         self.name_notify.grid(row=1, column=1, sticky='nsew')
 
         # Email
-        self.email_label = tk.Label(self, text="Email")
+        self.email_label = tk.Label(self.frmUser, text="Email")
         self.email_label.grid(row=2, column=0, columnspan=2, sticky='nsew')
 
-        self.email_entry = tk.Entry(
-            self, textvariable=self.email_text, width=5)
+        self.email_entry = tk.Entry(self.frmUser, textvariable=self.email_text, width=5)
         self.email_entry.grid(row=3, column=0, sticky='nsew')
 
-        self.email_notify = tk.Label(self, text="*")
+        self.email_notify = tk.Label(self.frmUser, text="*")
         self.email_notify.grid(row=3, column=1, sticky='nsew')
 
         # Telefone
-        self.telephone_label = tk.Label(self, text="Telefone")
+        self.telephone_label = tk.Label(self.frmUser, text="Telefone")
         self.telephone_label.grid(row=4, column=0, columnspan=2, sticky='nsew')
 
         # self.telephone_entry = tk.Entry(
-        #     textvariable=self.telephone_text)
+        #     self.frmUser, textvariable=self.telephone_text)
         # self.telephone_entry.grid(row=5, column=0, sticky='nsew')
 
-        self.telephone_entry = EntryPhoneNumber(self,
+        self.telephone_entry = EntryPhoneNumber(self.frmUser,
                                                 placeholder='(000)-1111-111111',
                                                 textvariable=self.telephone_text)
 
         self.telephone_entry.grid(row=5, column=0, sticky='nsew')
 
-        self.telephone_notify = tk.Label(self, text="*")
+        self.telephone_notify = tk.Label(self.frmUser, text="*")
         self.telephone_notify.grid(row=5, column=1, sticky='nsew')
 
         # Button
-        self.add_button = tk.Button(self, text="Adicionar",
-                                    command=self.add)
-
-        self.add_button.grid(row=6,
-                             column=0,
-                             columnspan=2,
-                             sticky='nsew',
-                             pady=20)
+        self.add_button = tk.Button(
+            self.frmUser, text="Adicionar", command=self.add)
+        self.add_button.grid(row=6, column=0, columnspan=2, sticky='nsew', pady=20)
 
         self.digits: str = ""
 
@@ -78,6 +77,22 @@ class UserView(tk.Frame):
 
     def digits_get(self) -> str:
         return self.digits
+
+    def event_key(self, event):
+
+        digit = str(event.char)
+
+        print("name_text:", self.name_text.get())
+
+        if (len(self.name_text.get()) <= 9):
+            return exit
+
+        
+
+        # self.digits_set(self.name_text.get())
+
+        # print("pressed", self.digits_get())
+
 
     def add(self) -> None:
 
